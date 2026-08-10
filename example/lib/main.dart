ec77ce063499c9a39ffa04b96295ff4f178a86a2
nahnah <nandanrmenon@gmail.com>
2026-08-10 20:27:01 +0100

    fix: revert themeMode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       darkTheme: HornbillTheme(
         appBarFontFamily: GoogleFonts.googleSansFlex().fontFamily,
       ).darkTheme(),
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       home: const HornbilExampleApp(),
     );
   }
